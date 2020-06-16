60cb2395779df8a343e907470fa75251dbf79c47
Josh Mu <mu@joshmu.com>
2020-06-16 13:49:38 +1000

    make query

--- app.js
+++ app.js
@@ -4,13 +4,15 @@
 require('dotenv').config()
 const mysql = require('mysql')
 
+// setup
 const con = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
+  host: 'localhost',
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  // database: process.env.MYSQL_DATABASE,
+  database: 'sitepoint',
 })
 
+// connect
 con.connect((err) => {
   if (err) {
     console.log('Error connecting to Db')
@@ -19,6 +21,15 @@ con.connect((err) => {
   console.log('Connection established')
 })
 
+// make a query
+con.query('SELECT * FROM authors', (err, rows) => {
+  if (err) throw err
+
+  console.log('Data received from Db:')
+  console.log(rows)
+})
+
+// connection closed
 con.end((err) => {
   // The connection is terminated gracefully
   // Ensures all remaining queries are executed
