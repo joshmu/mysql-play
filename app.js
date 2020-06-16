d7065a3ef12381b7e3b78a1cd80176de7f7f3ffe
Josh Mu <mu@joshmu.com>
2020-06-16 13:44:13 +1000

    connection handling

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+// following instructions from:
+// https://www.sitepoint.com/using-node-mysql-javascript-client/
+
 require('dotenv').config()
 const mysql = require('mysql')
 
-const connection = mysql.createConnection({
+const con = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  // database: process.env.MYSQL_DATABASE,
 })
 
-connection.connect((err) => {
-  if (err) throw err
-  console.log('Connected!')
+con.connect((err) => {
+  if (err) {
+    console.log('Error connecting to Db')
+    return
+  }
+  console.log('Connection established')
+})
+
+con.end((err) => {
+  // The connection is terminated gracefully
+  // Ensures all remaining queries are executed
+  // Then sends a quit packet to the MySQL server.
 })
