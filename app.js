309f01c7e7984ab1a81003cae55906f4ea05b711
Josh Mu <mu@joshmu.com>
2020-06-16 13:11:56 +1000

    initial

---
+++ app.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+})
+
+connection.connect((err) => {
+  if (err) throw err
+  console.log('Connected!')
+})
